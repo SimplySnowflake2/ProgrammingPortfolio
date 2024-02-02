01a793830874596d1841a77d31ef48f0e891b168
SimplySnowflake2 <142460052+SimplySnowflake2@users.noreply.github.com>
2024-02-02 11:28:00 -0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,39 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+import java.util.Random;
 
 class Main {
+  // 1. create a print statement to welcome the user to the game
   public static void main(String[] args) {
-    for (int bottles = 99; bottles > 2; bottles = bottles - 1) {
-      System.out.println(bottles
-          + " bottles of water on the wall, " + bottles + " bottles of water. Take one down and pass it around, "
-          + (bottles - 1) + " bottles of water on the wall.");
-      System.out.println("\n");
-    }
-    for (int bottle = 2; bottle > 1; bottle = bottle - 1) {
-      System.out.println(bottle
-          + " bottles of water on the wall, " + bottle + " bottles of water. Take one down and pass it around, "
-          + (bottle - 1) + " bottle of water on the wall.");
-      System.out.println("\n");
-    }
-    for (int bottle = 1; bottle > 0; bottle = bottle - 1) {
-      System.out.println(bottle
-          + " bottle of water on the wall, " + bottle + " bottle of water. Take one down and pass it around, "
-          + (bottle - 1) + " bottles of water on the wall.");
-      System.out.println("\n");
-    }
+    System.out.println("Welcome to the game! I am thinking of a number between 1 and 100, what do you think it is?");
+    // 2. create a scanner to take in the user's input
+    Scanner input = new Scanner(System.in);
+    // 3. create a variable to store the user's input
+    int userInput = 0;
+    // 4. create a variable to store the random number
+    Random rand = new Random();
+    int randomNumber = rand.nextInt(100) + 1;
+    // 5. create a variable to store the amount of iterations
+    int iterations = 0;
+    // 6. create a while loop that runs until the user's input is equal to the random number
+    while (userInput != randomNumber){
+      // 7. create conditional statements to check the values
+      userInput = input.nextInt();
+      if (userInput < randomNumber) {
+        System.out.println("Too low! Guess a higher number.");
+      } else if (userInput > randomNumber) {
+        System.out.println("Too high! Guess a lower number.");
+      }
+    iterations++;
+      }
+    // 9. create a print statement to tell the user the amount of iterations
+    System.out.println("Number of iterations: " + iterations);
+    // 10. create a print statement to tell the user that they won the game
+    System.out.println("Congratulations! You won the game!");
+    input.close();
   }
+
+  // @Test
+  // void addition() {
+  // assertEquals(2, 1 + 1);
+  // }
 }
